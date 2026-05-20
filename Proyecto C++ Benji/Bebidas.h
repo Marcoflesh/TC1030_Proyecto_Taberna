86fbe69aeea596fe18a159fb2c9913c8c8228ca9
Marcoflecha <istacu29@gmail.com>
2026-05-20 14:33:52 -0600

    Implementar Conjunto de Clases Bebidas y Personas

---
+++ Proyecto C++ Benji/Bebidas.h
@@ -0,0 +1,524 @@
+#ifndef BEBIDAS_H
+#define BEBIDAS_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// 1. Clase Abstracta
+class Bebida {
+protected:
+    int stock;
+    float porcentaje, precio, descuento;
+    string nombre, marca, pais, tipo_bebida;
+
+public:
+    Bebida(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre);
+
+    virtual ~Bebida(){}
+
+    int getStock() {return stock;}
+    float getGrados() {return porcentaje;}
+    float getPrecio() {return precio;}
+    float getDescuento() {return descuento;}
+    string getNombre() {return nombre;}
+    string getMarca() {return marca;}
+    string getPais() {return pais;}
+    string getTipoBebida() {return tipo_bebida;}
+
+    void sumarStock(int cantidad);
+    void restarStock(int loot);
+
+    virtual float descontar1(float total); 
+    virtual float descontar(int cantidad);
+    virtual void servir() = 0; 
+    virtual string toString();
+    virtual string toTexto() = 0;
+};
+
+Bebida::Bebida(string _tipo_bebida, int _stock, float _porc, float _precio, 
+        float _desc, const string& _marca, const string& _pais, 
+        const string& _nombre) : tipo_bebida(_tipo_bebida), stock(_stock), 
+        porcentaje(_porc), precio(_precio), descuento(_desc), marca(_marca), 
+        pais(_pais), nombre(_nombre) {}
+
+void Bebida::sumarStock(int cantidad) {
+    stock += cantidad;
+}
+
+void Bebida::restarStock(int cantidad) {
+    stock -= abs(cantidad);
+}
+
+float Bebida::descontar1(float total) {
+    float descontado =precio - (precio * descuento);
+    cout << "Se aplico un descuento a " << nombre << " del "
+         << (descuento * 100) << "%, nuevo precio: $" << descontado << endl;
+    return descontado;
+}
+
+float Bebida::descontar(int cantidad) {
+    if (cantidad <= 0) {
+        cout << "Cantidad invalida." << endl;
+        return 0.0;
+    }
+    float total = precio * cantidad;
+    cout << "Tomaste " << cantidad << " de " << nombre << endl;
+    return descontar1(total);
+}
+
+string Bebida::toString() {
+    string texto = "Bebida: " + nombre + "\nMarca: " + marca + "\nPais: " 
+        + pais + "\nPorcentaje de alcohol: " + to_string(porcentaje) 
+        + "\nPrecio: $" + to_string(precio) + "\nCon descuento de : " 
+        + to_string(descuento);
+    return texto;
+}
+
+// 2. Cerveza
+class Cerveza : public Bebida {
+private:
+    string tipo;
+    float ibu; 
+
+public:
+    Cerveza(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _tipo, float _ibu);
+    virtual ~Cerveza(){}
+    
+    string getTipo(){return tipo;}
+    float getIbu(){return ibu;}
+
+    void servir() override;
+    string toString() override;
+    float descontar(int cantidad) override;
+    string toTexto() override;
+};
+
+Cerveza::Cerveza(string _tipo_bebida, int _stock, float _porcentaje, 
+    float _precio, float _descuento, const string& _marca, const string& _pais,
+    const string& _nombre, const string& _tipo, float _ibu) : 
+    Bebida(_tipo_bebida, _stock, _porcentaje, _precio, _descuento, _marca, 
+        _pais, _nombre) {
+            tipo = _tipo;
+            ibu = _ibu;
+        }
+
+void Cerveza::servir() {
+    cout << "Sirviendo una " << nombre << " tipo " << tipo << "." << endl;
+}
+
+float Cerveza::descontar(int cantidad) {
+    if (cantidad < 5) {
+        descuento = 0.0;
+        cout << "No hay descuentos disponibles" << endl;
+    } else {
+        descuento = 0.2;
+        cout << "Tienes un 20% de descuento" << endl;
+    }
+    return Bebida::descontar(cantidad);
+}
+
+string Cerveza::toString() {
+    return Bebida::toString() + "\nTip: " + tipo + "\nIBU (amargor): "
+        + to_string(ibu);
+}
+
+string Cerveza::toTexto() {
+    string texto;
+    texto = "Cerveza," + to_string(stock) + "," + to_string(porcentaje)
+        + "," + to_string(precio) + "," + to_string(descuento)
+        + "," + marca + "," + pais + "," + nombre
+        + "," + tipo + "," + to_string(ibu);
+        return texto;
+}
+
+// 3. Mezcal
+class Mezcal : public Bebida {
+private:
+    string tipo_agave;
+    bool gusano;
+
+public:
+    Mezcal(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _tipo_agave, bool _gusano);
+    virtual ~Mezcal(){}
+
+    string getTipoAgave(){return tipo_agave;}
+    bool getGusano(){return gusano;}
+
+    void servir() override;
+    string toString() override;
+    float descontar(int cantidad) override;
+    string toTexto() override;
+};
+
+Mezcal::Mezcal(string _tipo_bebida, int _stock, float _porcentaje, 
+    float _precio, float _descuento, const string& _marca, const string& _pais, 
+    const string& _nombre, const string& _agave, bool _gusano) 
+        : Bebida(_tipo_bebida, _stock, _porcentaje, _precio, _descuento, 
+            _marca, _pais, _nombre)
+        {
+        tipo_agave = _agave;
+        gusano = _gusano;
+    }
+
+void Mezcal::servir() {
+    cout << "Aqui tienes un " << nombre << " de agave " << tipo_agave 
+    << "." << endl;
+    if (gusano) {
+        cout << "Este mezcal trae gusano de maguey." << endl;
+    }   
+}
+
+float Mezcal::descontar(int cantidad) {
+    float total = precio * cantidad;
+    if (tipo_agave == "Tabala" || tipo_agave == "Tepeztate" 
+        || tipo_agave == "Jabali") {
+        cout << "No hay descuentos para esta bebida" << endl;
+        cout << "Total: $" << total << endl;
+        return total;
+    }
+    if (cantidad > 7) {
+        descuento = 0.15;
+        cout << "Descuento del 15% aplicado." << endl;
+    }
+    return Bebida::descontar(cantidad);
+}
+
+string Mezcal::toString() {
+    string texto, g = gusano ? "con gusano" : "sin gusano";
+    texto = Bebida::toString() + "\nAgave: " + tipo_agave + "\n" + g;
+    return texto; 
+}
+
+string Mezcal::toTexto() {
+    string texto;
+    texto = "Mezcal," + to_string(stock) + "," + to_string(porcentaje)
+        + "," + to_string(precio) + "," + to_string(descuento) + "," + marca 
+        + "," + pais + "," + nombre + "," + tipo_agave + "," + 
+        to_string(gusano);
+    return texto;
+}
+
+// 4. Ron
+class Ron : public Bebida {
+private:
+    string estilo;
+    bool especias;
+
+public:
+    Ron(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _estilo, bool _especias);
+
+    virtual ~Ron(){}
+
+    string getEstilo(){return estilo;}
+    bool getEspecias(){return especias;}
+
+    void servir() override;
+    string toString() override;
+    float descontar(int cantidad) override;
+    string toTexto() override;
+};
+
+Ron::Ron(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _estilo, bool _especias) :
+        Bebida(_tipo_bebida, _stock, _porcentaje, _precio, _descuento, _marca, 
+            _pais, _nombre) {
+            estilo = _estilo;
+            especias = _especias;
+        }
+
+void Ron::servir() {
+        cout << "Aquí tienes un buen " << nombre << " " << estilo << endl;
+    if (especias){
+        cout << "Tómatelo en una cuba" << endl;
+    } else {
+        if (estilo == "Blanco") {
+            cout << "Tómatelo en un mojito" << endl;
+        } else if (estilo == "Añejo" || estilo == "Gran Reserva") {
+            cout << "Tómalo solo con hielo" << endl;
+        } else {
+            cout << "Tómalo con jugo de cualquier fruta" << endl;
+        }
+    }
+}
+
+string Ron::toString() {
+    string especiado = especias ? "especiado" : "puro", texto;
+    texto = Bebida::toString() + "\nEstilo de ron: " + estilo + "\nTipo: " 
+    + especiado;
+    return texto;
+}
+
+float Ron::descontar(int cantidad) {
+    if (estilo == "Añejo" || estilo == "Gran Reserva") {
+        float total = precio * cantidad;
+        cout << "Este ron " << estilo << " al ser premium no aplica descuentos" 
+        << endl;
+        cout << "Tienes que pagar $" << total << endl;
+        return total;
+    }
+    return Bebida::descontar(cantidad);
+}
+
+string Ron::toTexto() {
+    string texto;
+    texto = "Ron," + to_string(stock) + "," + to_string(porcentaje)
+        + "," + to_string(precio) + "," + to_string(descuento) + "," + marca 
+        + "," + pais + "," + nombre + "," + estilo + "," + to_string(especias);
+    return texto;
+}
+
+// 5. Tequila
+class Tequila : public Bebida {
+private:
+    string region, categoria;
+    bool agave;
+
+public:
+    Tequila(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _region, const string& _categoria, 
+        bool _agave);
+    virtual ~Tequila(){}
+    
+    string getRegion(){return region;}
+    string getCategoria(){return categoria;}
+    bool isAgave(){return agave;}
+
+    void servir() override;
+    string toString() override; 
+    float descontar(int cantidad) override;
+    string toTexto() override;
+};
+
+Tequila::Tequila(string _tipo_bebida, int _stock, float _porcentaje, 
+        float _precio, float _descuento, const string& _marca, 
+        const string& _pais, const string& _nombre, const string& _region, 
+        const string& _categoria, bool _agave) 
+        : Bebida(_tipo_bebida, _stock, _porcentaje, _precio, _descuento, 
+            _marca, _pais, _nombre) {
+            region = _region;
+            categoria = _categoria;
+            agave = _agave; 
+        }
+
+void Tequila::servir() {
+    cout << "Estas tomando tequila " << categoria <<  " de " << region << endl;
+    if (categoria == "Blanco") {
+        cout << "Tómalo en seco" << endl;
+    } else {
+        cout << "Tómalo a tragos para un mejor disfrute" << endl;
+    }
+}
+
+string Tequila::toString() {
+    string pureza = agave ? "100% agave" : "mixto", texto;
+    texto = Bebida::toString() + "\nRegion: " + region + "\nCategoria: " 
+    + categoria + "\nHecho 100% de agave: " + pureza; 
+    return texto;
+}
+
+float Tequila::descontar(int cantidad) {
+    float total = precio * cantidad;
+    if (agave){
+        cout << "No se encontraron descuentos disponibles" << endl;
+        cout << "Total: $" << total << endl;
+        return total;
+    } else {
+        if (cantidad > 7) return Bebida::descontar(cantidad);
+        else {
+            cout << "Total: $" << total << endl;
+            return total;
+        }
+    }
+}
+
+string Tequila::toTexto() {
+    string texto;
+    texto = "Tequila," + to_string(stock) + "," + to_string(porcentaje)
+        + "," + to_string(precio) + "," + to_string(descuento) + "," + marca 
+        + "," + pais + "," + nombre + "," + region + "," + categoria + "," 
+        + to_string(agave);
+        return texto;
+}
+
+// 6. Vino
+class Vino : public Bebida {
+private:
+    string uva;
+    int year_cosecha;
+
+public:
+    Vino(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _uva, int _year_cosecha);
+    virtual ~Vino(){}
+    
+    string getUva(){return uva;}
+    int getYearCosecha(){return year_cosecha;}
+
+    void servir() override;
+    string toString() override;
+string toTexto() override;
+};
+
+Vino::Vino(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _uva, int _year_cosecha) : 
+        Bebida(_tipo_bebida, _stock, _porcentaje, _precio, _descuento, _marca, 
+            _pais, _nombre) {
+                uva = _uva;
+                year_cosecha = _year_cosecha;
+            }
+
+void Vino::servir() {
+    cout << "Una copa del vino " << nombre << ", cosecha " << year_cosecha 
+    << endl;
+}
+
+string Vino::toString() {
+        string texto;
+        texto = Bebida::toString() + "\nTipo de uva: " + uva + 
+        "\nAño de cosecha: " + to_string(year_cosecha);
+    return texto;
+}
+
+string Vino::toTexto() {
+    string texto;
+    texto = "Vino," + to_string(stock) + "," + to_string(porcentaje)
+        + "," + to_string(precio) + "," + to_string(descuento) + "," + marca 
+        + "," + pais + "," + nombre + "," + uva + "," + 
+        to_string(year_cosecha);
+    return texto;
+}
+
+// 7. Vodka
+class Vodka : public Bebida {
+private:
+    string sabor;
+    int destilaciones;
+
+public:
+    Vodka(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _sabor, int _destilaciones);
+    virtual ~Vodka(){}
+
+    string getSabor(){return sabor;}
+    int getDestilaciones(){return destilaciones;}
+
+    void servir() override;
+    string toString() override;
+    float descontar(int cantidad) override;
+    string toTexto() override;
+};
+
+Vodka::Vodka(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, const string& _sabor, int _destilaciones) 
+        : Bebida(_tipo_bebida, _stock, _porcentaje, precio, _descuento, _marca, 
+            _pais, _nombre) {
+                sabor = _sabor;
+                destilaciones = _destilaciones;
+            }
+
+void Vodka::servir() {cout << "Ten un vodka de " << sabor << endl;}
+
+string Vodka::toString()  {
+        string texto = Bebida::toString() + "\nSabor: " + sabor 
+    + "\nNúmero de destilaciones: " + to_string(destilaciones); 
+    return texto;
+}
+
+float Vodka::descontar(int cantidad) {
+    if (cantidad <= 4 || destilaciones >= 5){
+        float total = precio * cantidad;
+        cout << "Total: $" << total << endl;
+        return total;
+    } else if(cantidad >= 5 && destilaciones < 5){
+        cout << "Se aplicó un 18% de descuento" << endl;
+        descuento = 0.18;
+        return Bebida::descontar(cantidad);
+    }
+    return Bebida::descontar(cantidad);
+}
+
+string Vodka::toTexto() {
+    string texto;
+    texto = "Vodka," + to_string(stock) + "," + to_string(porcentaje)
+        + "," + to_string(precio) + "," + to_string(descuento) + "," + marca 
+        + "," + pais + "," + nombre + "," + sabor + "," + 
+        to_string(destilaciones);
+    return texto;
+}
+
+// 8. Whiskey
+class Whiskey : public Bebida {
+private:
+    int years_aged; 
+    string barrica;
+
+public:
+    Whiskey(string _tipo_bebida, int _stock, float _porcentaje, float _precio, 
+        float _descuento, const string& _marca, const string& _pais, 
+        const string& _nombre, int _years_aged, const string& _barrica);
+    virtual ~Whiskey(){}
+    
+    int getYearsAged() {return years_aged;}
+    string getBarrica() {return barrica;}
+
+    void servir() override;
+    string toString() override;
+    float descontar(int cantidad) override;
+    string toTexto() override;
+};
+
+Whiskey::Whiskey(string _tipo_bebida, int _stock, float _porcentaje, 
+        float _precio, float _descuento, const string& _marca, 
+        const string& _pais, const string& _nombre, int _years_aged, 
+        const string& _barrica) : Bebida(_tipo_bebida, _stock, _porcentaje, 
+            _precio, _descuento, _marca, _pais, _nombre) {
+                years_aged = _years_aged;
+                barrica = _barrica;
+            }
+
+void Whiskey::servir() {
+    cout << "Ten un whiskey de " << years_aged 
+        << " años de añejamiento de una barrica " << barrica << endl;
+}
+
+string Whiskey::toString() {
+        string texto = Bebida::toString() + "\nAños de añejamiento: " 
+    + to_string(years_aged) + "\nTipo de barrica: " + barrica;
+    return texto;
+}
+
+float Whiskey::descontar(int cantidad) {
+    if (years_aged > 20) {
+        float total = precio * cantidad;
+        cout << "Total: $" << total << endl;
+        return total;
+    }
+    return Bebida::descontar(cantidad);
+}
+
+string Whiskey::toTexto() {
+    string texto;
+    texto = "Whiskey," + to_string(stock) + "," + to_string(porcentaje)
+        + "," + to_string(precio) + "," + to_string(descuento) + "," + marca 
+        + "," + pais + "," + nombre + "," + to_string(years_aged) 
+        + "," + barrica;
+    return texto;
+}
+
+#endif
---
+++ Proyecto C++ Benji/Personas.h
@@ -0,0 +1,205 @@
+#ifndef PERSONAS_H
+#define PERSONAS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// 1. Clase Abstracta
+class Persona {
+protected:
+    string nombre, apellido, curp, direccion, nacionalidad;
+    int no_tarjeta, telefono, edad;
+
+public:
+    Persona(const string& _nombre, const string& _apellido, 
+        const string& _curp, string _direccion, const string& _nacionalidad, 
+        int _no_tarjeta, int _telefono, int _edad);
+    virtual ~Persona(){}
+
+    string getNombre() {return nombre;}
+    string getApellido() {return apellido;}
+    string getCurp() {return curp;}
+    string getDireccion() {return direccion;}
+    string getNacionalidad() {return nacionalidad;}
+    int getTarjeta() {return no_tarjeta;}
+    int getTelefono() {return telefono;}
+    int getEdad() {return edad;}
+
+    void setDireccion(string direction) {direccion = direction;}
+    void setTarjeta(int card) {no_tarjeta = card;}
+    void setTelefono(int phone) {telefono = phone;}
+    void setEdad(int age) { edad = age;}
+
+    virtual void mostrar_info();
+};
+
+Persona::Persona(const string& _nombre, const string& _apellido, 
+        const string& _curp, string _direccion, const string& _nacionalidad, 
+        int _no_tarjeta, int _telefono, int _edad) : nombre(_nombre), 
+        apellido(_apellido), curp(_curp), direccion(_direccion),
+        nacionalidad(_nacionalidad), no_tarjeta(_no_tarjeta), 
+        telefono(_telefono), edad(_edad) {}
+
+void Persona::mostrar_info() {
+    cout << "Nombre: " << nombre << " " << apellido << endl;
+    cout << "Curp: " << curp << endl;
+    cout << "Dirección: " << direccion << endl;
+    cout << "Nacionalidad: " << nacionalidad << endl;
+    cout << "Tarjeta: " << no_tarjeta << endl;
+    cout << "Teléfono: " << telefono << endl;
+    cout << "Edad: " << edad << endl;
+}
+
+// 2. Cliente
+class Cliente: public Persona {
+private:
+    int id, tragos;
+    double cuenta;
+    string preferencia_trago;
+
+public:
+    Cliente(const string& _nombre, const string& _apellido, 
+        const string& _curp, string _direccion, const string& _nacionalidad, 
+        int _no_tarjeta, int _telefono, int _edad, 
+        const string& _preferencia_trago);
+
+    Cliente(const string& _nombre, const string& _apellido, const string& _curp,
+        string _direccion, const string& _nacionalidad, int _tarjeta,
+        int _telefono, int _edad, const string& _preferencia, int _id, 
+        int _tragos, double _cuenta);
+    
+    virtual ~Cliente(){}
+
+    int getCliente(){return id;}
+    int getTragos(){return tragos;}
+    double getCuenta(){return cuenta;}
+    string getPreferenciaTrago(){return preferencia_trago;}
+
+    void setTragos(int drinks) {tragos = drinks;}
+    void setCuenta(double bill) {cuenta = bill;}
+    void setPreferenciaTrago(string beverage_preference) {preferencia_trago = 
+        beverage_preference;}
+
+    void registro_consumo(double precio_trago);
+    void mostrar_info() override;
+    void pagar_cuenta();
+};
+
+Cliente::Cliente(const string& _nombre, const string& _apellido, 
+    const string& _curp, string _direccion, const string& _nacionalidad,
+    int _tarjeta, int _telefono, int _edad, const string& _preferencia)
+    : Persona(_nombre, _apellido, _curp, _direccion, _nacionalidad,
+        _tarjeta, _telefono, _edad) {
+        preferencia_trago = _preferencia;
+        tragos  = 0;
+        cuenta  = 0.0;
+        id      = 100000 + rand() % 900000;
+}
+
+Cliente::Cliente(const string& _nombre, const string& _apellido, 
+    const string& _curp, string _direccion, const string& _nacionalidad,
+    int _tarjeta, int _telefono, int _edad, const string& _preferencia, 
+    int _id, int _tragos, double _cuenta) : Persona(_nombre, _apellido, _curp, 
+        _direccion, _nacionalidad, _tarjeta, _telefono, _edad) {
+    preferencia_trago = _preferencia;
+    id     = _id;
+    tragos = _tragos;
+    cuenta = _cuenta;
+}
+
+void Cliente::registro_consumo(double precio_trago) {
+    if (edad < 18){
+        cout << "Prohibido la venta a menores" << endl;
+        return;
+    }
+    if (precio_trago > 0){
+        tragos++;
+        cuenta += precio_trago;
+        cout << nombre << " LLevas # " << tragos << endl;
+        cout << "Subtotal: $" << cuenta << endl;
+    }
+}
+
+void Cliente::mostrar_info() {
+    cout << "Detalles del cliente" << endl;
+    Persona::mostrar_info();
+    cout << "Id " << id << endl;
+    cout << "Preferencias de Bebidas " << preferencia_trago << endl;
+    cout << "Tragos tomados" << tragos << endl;
+}
+
+void Cliente::pagar_cuenta() {
+    if(tragos == 0 && cuenta <= 0.0){
+        cout << nombre << "No tienes deuda pendiente." << endl;
+        return;
+    }
+    char opcion;
+    do{
+        cout << "ID cliente: " << id << " Nombre: " << nombre << endl;
+        cout << "Tragos Tomados: " << tragos << endl;
+        cout << "SUBTOTAL: $" << cuenta << endl;
+        cout << "IVA: 16%" << endl;
+        cout << "TOTAL: $" << (cuenta * 1.16) << endl;
+        cout << "¿Desea pagar? (s/n): ";
+        cin >> opcion;
+        if (opcion == 's' || opcion == 'S') {
+            cout << "Pago realizado." << endl;
+            tragos = 0.0;
+            cuenta = 0.0;
+            cout << "Muchas gracias por su compra " << nombre << endl;
+        } else {
+            cout << "No se registró bien su respuesta" << endl;
+        }
+    } while (opcion != 's' && opcion != 'S');
+}
+
+// 3. Empleado
+class Empleado: public Persona {
+private:
+    private:
+    double sueldo_base;
+    int horas_trabajo; 
+    string puesto;
+
+public:
+    Empleado(const string& _nombre, const string& _apellido, 
+    const string& _curp, string _direccion, const string& _nacionalidad, 
+    int _tarjeta, int _telefono, int _edad, double _sueldo, int _horas_trabajo, 
+    string _puesto);
+
+    virtual ~Empleado(){}
+
+    double getSueldoBase() {return sueldo_base;}
+    int getHoras() {return horas_trabajo;}
+    string getPuesto() {return puesto;}
+
+    void setSueldoBase(double salary) {sueldo_base = salary;}
+    void setHorasTrabajo(int hours) {horas_trabajo = hours;}
+    void setPuesto(string position) {puesto = position;}
+
+    double calculo_nomina();
+    void mostrar_info() override;
+};
+
+Empleado::Empleado(const string& _nombre, const string& _apellido, 
+        const string& _curp, string _direccion,  const string& _nacionalidad, 
+        int _tarjeta, int _telefono, int _edad, double _sueldo, 
+        int _horas_trabajo, string _puesto) : Persona(_nombre, _apellido, 
+            _curp, _direccion, _nacionalidad, _tarjeta, _telefono, _edad) {
+    sueldo_base        = _sueldo;
+    horas_trabajo      = _horas_trabajo;
+    puesto             = _puesto;
+}
+
+double Empleado::calculo_nomina() {return sueldo_base * horas_trabajo;}
+
+void Empleado::mostrar_info() {
+    Persona::mostrar_info();
+    cout << "Puesto: " << puesto << endl;
+    cout << "Sueldo: $" << sueldo_base << endl;
+    cout << "Horas: " << horas_trabajo << endl;
+    cout << "Nómina: $" << calculo_nomina() << endl; 
+}
+
+#endif
